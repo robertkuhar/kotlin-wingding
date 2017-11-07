cb8e95b8af150284a39bd21953848eb42b9b1a6c
robert.kuhar <robert.kuhar@climate.com>
2017-11-06 22:52:11 -0800

    Functions and Lambdas through Lambdas

---
+++ src/main/kotlin/funcs_and_lambdas/functions.kt
@@ -0,0 +1,34 @@
+package funcs_and_lambdas
+
+/**
+ * Doubles the input value of [x].
+ * @return 2 * [x].
+ */
+fun doubleIt(x: Int): Int {
+    return 2 * x
+}
+
+fun cat(vararg strings: String): String {
+    var cat = ""
+    strings.forEach { cat = cat + it }
+    return cat
+}
+
+infix fun Int.plus(x: Int): Int {
+    return this + x
+}
+
+fun main(args: Array<String>) {
+    val x = 2
+    println("doubleIt($x) returns ${doubleIt(2)}")
+
+    println("${cat("a","b")}")
+    val catViaSpread = cat(*arrayOf("c", "a", "t"))
+    println("$catViaSpread")
+
+    val infixOneplusTwo = 1 plus 2
+    val postOneplusTwo = 1.plus(2)
+    println("$infixOneplusTwo\n$postOneplusTwo")
+
+
+}
