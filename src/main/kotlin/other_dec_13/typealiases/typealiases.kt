fb1fae2291e3783314633e1efd9a1302e697df07
robert.kuhar <robert.kuhar@climate.com>
2018-01-10 09:41:34 -0800

    Aliens

--- src/main/kotlin/other_dec_13/typealiases/typealiases.kt
+++ src/main/kotlin/other_dec_13/typealiases/typealiases.kt
@@ -6,6 +6,14 @@ data class Car(val make: Manufacturer, val model: String, val year: Int)
 
 typealias CarSet = Set<Car>
 
+typealias OscarWinners = Map<String, String>
+fun countLaLaLand(oscarWinners: OscarWinners) =
+        oscarWinners.count { it.value.contains("La La Land") }
+
+// Note that the type names (initial and the type alias) are interchangeable:
+fun checkLaLaLandIsTheBestMovie(oscarWinners: Map<String, String>) =
+        oscarWinners["Best picture"] == "La La Land"
+
 class A {
     inner class Inner
 }
@@ -13,9 +21,8 @@ class A {
 class B {
     inner class Inner
 }
-
+// This is of dubious value...
 typealias AInner = A.Inner
-
 typealias BInner = B.Inner
 
 typealias Predicate<T> = (T) -> Boolean
